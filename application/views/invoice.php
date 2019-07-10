1bb91364a2da838d58d574f6491d192d49479c53
JerryHernandez <info@mylistingpitch.com>
2019-07-10 11:44:46 +0530

    Invoice PDF design and color changes

--- application/views/invoice.php
+++ application/views/invoice.php
@@ -11,7 +11,7 @@
 <table width="100%" border="0" cellpadding="0" cellspacing="0" align="center" bgcolor="#ecf0f1"style="background-color: rgb(255,255,255);">
 	<tr>
 		<td bgcolor="#ecf0f1"align="center" style="background-color: rgb(255,255,255); padding-top:140px;">
-			<table width="600" border="0" cellpadding="0" cellspacing="0" align="center" >
+			<table width="600" border="0" cellpadding="0" cellspacing="0" align="center" style="padding:35px; border:1px solid black;">
 				<tr>
 					<td width="600" align="center">
                                             
@@ -25,8 +25,9 @@
 				</tr>
 				<tr>
 					<td valign="middle" align="center" width="600"style="padding-top:1px; " >
+						<br/>
 						<span style="text-align: center; font-size: 12px;     font-family: Montserrat; color: rgb(0,0,0); font-weight: normal; text-transform: uppercase; line-height: 18px; margin-bottom:25px;">Your Report Has Been Created.</span>
-						<br><br>
+						<br/><br/><br/>
 					</td>
 				</tr>
 				<tr>
@@ -49,14 +50,14 @@
 								<td valign="top" width="150" align="center" style="padding-left:20px" >
 									<table width="150" border="0" cellpadding="0" cellspacing="0" align="center" style="border-collapse:collapse; mso-table-lspace:0pt; mso-table-rspace:0pt;">
 										<tr>
-											<td width="150" bgcolor="#f15d3e"align="center" style="border-top-left-radius: 3px; border-top-right-radius: 3px; border-bottom-right-radius: 3px; border-bottom-left-radius: 3px; background:none; border: 1px solid #000;">												
-												<table width="150" border="0" cellpadding="0" cellspacing="0" align="center">
+											<td width="150" bgcolor="#f15d3e"align="center" style="border-top-left-radius: 3px; border-top-right-radius: 3px; border-bottom-right-radius: 3px; border-bottom-left-radius: 3px; background:none; border: 1px solid #fff;">												
+												<table width="150" border="0" cellpadding="0" cellspacing="0" align="center" style="color: rgb(255, 255, 255);background-color: rgb(204,153,100);">
 													<tr>
 														<td width="100%" height="10" style="font-size: 1px; line-height: 1px;">&nbsp;</td>
 													</tr>
 													<tr>
-														<td valign="top" width="100%" align="center" style="text-align: center; font-size: 14px; font-family: Montserrat; color: rgb(0,0,0); font-weight: bold; line-height: 18px; text-transform: uppercase;" >
-															<span style="font-family: Montserrat; font-weight: normal;"><a href="<?php echo site_url(); ?>" style="text-decoration: none; color: rgb(0,0,0);">ACCOUNT LOGIN</a></span>
+														<td valign="top" width="100%" align="center" style="text-align: center; font-size: 14px; font-family: Montserrat; color: rgb(255, 255, 255); font-weight: bold; line-height: 18px; text-transform: uppercase;" >
+															<span style="font-family: Montserrat; font-weight: normal;"><a href="<?php echo site_url(); ?>" style="text-decoration: none; color: rgb(255, 255, 255);">ACCOUNT LOGIN</a></span>
 														</td>
 													</tr>
 													<tr>
@@ -75,8 +76,8 @@
 					<td style="line-height:10px;">&nbsp;</td>
 				</tr>
 				<tr>
-					<td width="600" bgcolor="#000" height="60" valign="middle" align="center" style="text-align:left; border-top-right-radius: 3px; border-top-left-radius: 3px; background-color: rgb(0,0,0);">
-							<span style="padding-left:25px; font-weight: normal; font-size: 14px; font-weight:800;     font-family: Montserrat; color: rgb(255, 255, 255); line-height: 24px; text-transform: uppercase;">Order & Payment Details:</span>														
+					<td width="600" bgcolor="#cc9964" height="60" valign="middle" align="center" style="text-align:left; border-top-right-radius: 3px; border-top-left-radius: 3px; background-color: rgb(204,153,100);">
+							<span style="padding-left:25px; font-weight: normal; font-size: 14px; font-weight:800;     font-family: Montserrat; color: rgb(255, 255, 255); line-height: 24px; text-transform: uppercase;">Order & Payment Details</span>														
 					</td>
 				</tr>
 				<tr>
@@ -90,7 +91,7 @@
 										</span>
 										<br>
 										<span style="font-family: Montserrat font-weight: normal;">
-											<span style="color: rgb(85, 201, 224);"><?php echo $lp_details['property_address']; ?></span>
+											<span style="color: rgb(163,109,52);"><?php echo $lp_details['property_address']; ?></span>
 										</span>
 									</span>
 								</td>
@@ -145,7 +146,7 @@
 					<td height="5" style="line-height:5px;">&nbsp;</td>
 				</tr>
 				<tr>
-					<td width="600" border="0" height="60" cellpadding="0" cellspacing="0" align="center" bgcolor="#000"style="border-top-left-radius: 3px; border-top-right-radius: 3px; border-bottom-right-radius: 3px; border-bottom-left-radius: 3px; background-color: rgb(0,0,0);     font-family: Montserrat; font-size: 13px; color: rgb(255, 255, 255);">
+					<td width="600" border="0" height="60" cellpadding="0" cellspacing="0" align="center" bgcolor="#cc9964"style="border-top-left-radius: 3px; border-top-right-radius: 3px; border-bottom-right-radius: 3px; border-bottom-left-radius: 3px; background-color: rgb(204,153,100);     font-family: Montserrat; font-size: 13px; color: rgb(255, 255, 255);">
 						<span style="font-family: Montserrat font-weight: normal;">
 							<span style="color:#FFFFFF !important;" >&copy; <?php echo date("Y") ?> Modern Agent All rights Reserved</span>
 						</span>
