9d77a6d8c023c42ecf760441ff037aed013f8041
DESKTOP-OHG751Q\gerar <info@mylistingpitch.com>
2019-07-09 23:44:34 -0700

    First Change - Test.

--- application/views/reports/english/buyer/pages/4.php
+++ application/views/reports/english/buyer/pages/4.php
@@ -16,7 +16,7 @@
 				</div>
 			
 				<div class="textBlock">
-					<h1 class="underline-white">Why a 1-Mile Radius</h1>
+					<h1 class="underline-white">Why a 1/4 Mile Radius</h1>
 					<p>
 						This is an aerial view of the neighborhood in which you prospective property is located. This will give you the opportunity to get a birds eye view of any local parks, major streets, & highways.
 					</p>	
