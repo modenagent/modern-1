b9e2fc15b1bae240ff229a3f76982f3de81f6335
JerryHernandez <info@mylistingpitch.com>
2021-02-11 19:10:00 +0530

    Removed div from template

--- application/views/reports/english/seller/pages/5d.php
+++ application/views/reports/english/seller/pages/5d.php
@@ -7,9 +7,7 @@
         </header>
 
         <div class="">
-            <?php 
-echo "<pre>"; print_r($_comparables); exit;
-            ?>
+           
         <?php if(sizeof($_comparables)>0): ?>
             <?php $avaiProperty = 0; ?>
             <?php foreach ($_comparables as $key => $item): ?>
@@ -28,44 +26,9 @@ echo "<pre>"; print_r($_comparables); exit;
                         <div class="map1" id='map1'>
                         </div>
 
-                        <div class="btn-group price-button-group">
+                        
 
-                            <button type="button" style="font-size:17px; font-family: montserrat; letter-spacing:2px; border-color:<?php echo $theme; ?>; background-color:<?php echo $theme; ?>;" class="btn btn-default pull-left"> SALE PRICE </button>
-                            <button type="button" style="font-size:17px; border-color:<?php echo $theme; ?>;"class="btn btn-warning"> <?php echo $item['Price']; ?> </button>
-
-                        </div>
-
-                        <table class="table table-top-sales">
-                            <tbody>
-                                <tr class="gray-color">
-                                        <td style="padding-right: 80px;">Sold Date</td>
-                                        <td> Dist. </td>
-                                        <td> Sqft. </td>
-                                        <td> $/Sqft </td>
-                                </tr>
-
-                                <tr class="text-mybold">
-                                        <td style="padding-right: 80px;"> <?php echo $item['Date']; ?> </td>
-                                        <td> <?php echo $item['Distance']; ?> </td>
-                                        <td> <?php echo $item['SquareFeet']; ?> </td>
-                                        <td> $<?php echo $item['PricePerSQFT']; ?> </td>
-                                </tr>
-
-                                <tr class="gray-color">
-                                        <td style="padding-right: 80px;">Bed/Bath</td>
-                                        <td> Year Bit </td>
-                                        <td> Lot Area </td>
-                                        <td> Pool </td>
-                                </tr>
-
-                                <tr class="text-mybold">
-                                        <td style="padding-right: 80px;"> <?php echo $item['Beds']; ?>/<?php echo $item['Baths']; ?> </td>
-                                        <td><?php echo $item['Year']; ?></td>
-                                        <td><?php echo $item['LotSize']; ?></td>
-                                        <td><?php echo $item['Pool']; ?></td>
-                                </tr>
-                            </tbody>
-                        </table>
+                        
 
                     </div>
                 <?php if((($avaiProperty) % 2) || ($avaiProperty+1) == count($_comparables['comparable']) || $key==8):?><!-- End row div when even number or it is last item -->
