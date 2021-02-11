5ff1f9b3df52aff22e46cf5aeb8f8ccb456638c1
JerryHernandez <info@mylistingpitch.com>
2021-02-11 19:58:42 +0530

    Removed message

--- application/views/reports/english/seller/pages/5c.php
+++ application/views/reports/english/seller/pages/5c.php
@@ -1,8 +1,3 @@
-<?php 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-?>
 <?php if(sizeof($areaSalesAnalysis['comparable'])>0): ?>
 <?php 
 $avaiProperty = 0; //echo count($areaSalesAnalysis)."<br>";
