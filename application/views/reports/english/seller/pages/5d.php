f4a982126c1f4f3a16800556c460eadb61a51972
JerryHernandez <info@mylistingpitch.com>
2021-02-11 19:19:51 +0530

    Display data

--- application/views/reports/english/seller/pages/5d.php
+++ application/views/reports/english/seller/pages/5d.php
@@ -7,10 +7,11 @@
         </header>
 
         <div class="">
-           
         <?php if(sizeof($_comparables)>0): ?>
             <?php $avaiProperty = 0; ?>
-            <?php foreach ($_comparables as $key => $item): ?>
+            <?php foreach ($_comparables as $key => $item): 
+                echo "<pre>"; print_r($item); exit;
+                ?>
                 <?php 
                     if($key>8){
                         break;
@@ -19,18 +20,7 @@
                 <?php if(($avaiProperty+1) % 2):?><!-- Start row div when odd number -->
                 <div class="row" >
                 <?php endif; ?>
-                    <div class="col-xs-6">
-
-                        <h5 class="arrow"> <?php echo $item['Address']; ?> </h5>
-
-                        <div class="map1" id='map1'>
-                        </div>
-
-                        
-
-                        
-
-                    </div>
+                    
                 <?php if((($avaiProperty) % 2) || ($avaiProperty+1) == count($_comparables['comparable']) || $key==8):?><!-- End row div when even number or it is last item -->
                 </div>
                 <?php endif; ?>
