3c12319fc5b9ac2fa03d6217d0497792f4e45955
JerryHernandez <info@mylistingpitch.com>
2021-02-11 16:43:22 +0530

    Display data

--- application/views/reports/english/seller/index.php
+++ application/views/reports/english/seller/index.php
@@ -100,7 +100,16 @@
     }
 </style>   
 
-
+<?php 
+    if($partner && count($partner)>1) {
+        $this->load->view('reports/english/seller/pages/1');
+        $this->load->view('reports/english/seller/pages/1_multiagent');
+    } else if($partner && count($partner)==1) {
+        $this->load->view('reports/english/seller/pages/1_agent');
+    } else {
+        $this->load->view('reports/english/seller/pages/1');
+    }
+?>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js" type="text/javascript"></script>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js" type="text/javascript"></script>
