a0c70c3ca1dcbe399a92b8352d9e6f472afb27a5
Binita Desai <binita.s@crestinfosystems.net>
2021-02-17 17:28:04 +0530

    Revert Debug saml

--- simplesaml/www/index.php
+++ simplesaml/www/index.php
@@ -1,14 +1,6 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
-die("IN");
 require_once('_include.php');
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 
 $config = \SimpleSAML\Configuration::getInstance();
 
