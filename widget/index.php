78cb1f8e65b31876104424c58176104f96425fca
JerryHernandez <info@mylistingpitch.com>
2021-02-19 18:42:40 +0530

    Removed static url

--- widget/index.php
+++ widget/index.php
@@ -9,7 +9,7 @@ $user_id = 82;
 		<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery-1.9.1.min.js"></script>
 		<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true&libraries=places&key=AIzaSyDQQthVgLzHIRTyLS1WGP2spIshpD28n8M"></script>
 		<script type="text/javascript">
-	        var base_url = 'http://dev.modernagent.io/';
+	        var base_url = '<?php echo base_url(); ?>';
 	        var user_id = '<?php echo $user_id; ?>';
 	    </script>
 		<script type="text/javascript" src="cma.js"></script>
