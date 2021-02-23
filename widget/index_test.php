5979b905efc97d8fb2ce841050de24c33d7c18a4
Binita Desai <binita.s@crestinfosystems.net>
2021-02-23 16:23:18 +0530

    Test debug

--- widget/index_test.php
+++ widget/index_test.php
@@ -1,14 +1,10 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
 include('../simplesaml/lib/_autoload.php');
 if(empty($_GET['site_id'])) {
     echo "Invalid request";die;
 }
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
 $auth_id = $_GET['site_id'];
 
 if(!empty($auth_id)) {
@@ -37,27 +33,8 @@ else
     $root_dir = str_replace('\\', '/', $root_dir);
     define('FCPATH', $root_dir);
     include('index_ci.php');
-
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
-
-
-    $CI =& get_instance();
-    
     ob_end_clean();
-    
-    die("IN");
-?>
-<div id="cma-widget-container">
-	
-</div>
-<script type="text/javascript">
-    var app_main_url = "//<?=$_ENV['APP_DOMAIN']?>/SAMLConfig/index/<?=$auth_id?>";
-    console.log(app_main_url);
-</script>
-<?php
-// var_dump($attributes);
+    $CI =& get_instance();
 
     if(!empty($attributes['email']) && !empty($attributes['email'][0])) {
         $email = $attributes['email'][0];
@@ -82,6 +59,13 @@ else
 
 
 ?>
-<!-- <script type="text/javascript" src="cma_test.js"></script> -->
+<div id="cma-widget-container">
+	
+</div>
+<script type="text/javascript">
+    var app_main_url = "//<?=$_ENV['APP_DOMAIN']?>/SAMLConfig/index/<?=$auth_id?>";
+    console.log(app_main_url);
+</script>
+<script type="text/javascript" src="cma_test.js"></script>
 <?php }
 ?>
