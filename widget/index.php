d70782add1b82b0182824dc41abd9d26171f32d0
Binita Desai <binita.s@crestinfosystems.net>
2021-02-25 18:10:13 +0530

    Debuf issue

--- widget/index.php
+++ widget/index.php
@@ -159,8 +159,10 @@ else
         $session = SimpleSAML_Session::getSessionFromRequest();
         $session->cleanup();
 
+
         $sessionData = $CI->session->set_userdata($newdata);
         $_SESSION['userdata'] = $newdata;
+        var_dump($_SESSION);die;
         
     }
     else {
