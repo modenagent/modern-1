643fcc77e415b59a2b69f7ea9ab6b7463e1be3b7
JerryHernandez <info@mylistingpitch.com>
2021-02-25 19:22:59 +0530

    Added static page

--- application/views/reports/english/seller/pages/1.php
+++ application/views/reports/english/seller/pages/1.php
@@ -1,36 +1,35 @@
 <div class="container">
-    <div class="pdf1">
-        <div class="header">
-            <img src="<?php echo base_url().'assets/reports/english/seller/images/1/light-house.png';?>" alt="light-house" border="0">
-            <img src="<?php echo base_url().'assets/reports/english/seller/images/1/line.png';?>" alt="line" border="0">
-            <div class="text">
-                <h1>Sellers Report</h1>
-                <p><?php echo $property->PropertyProfile->SiteAddress ; ?>,</p>
-                <p><?php echo $property->PropertyProfile->SiteCity; ?>, <?php echo $property->PropertyProfile->SiteState ; ?> <?php echo $property->PropertyProfile->SiteZip; ?></p>
-            </div>
-        </div>
-        <div class="body">
-            <img src="<?php echo base_url().'assets/reports/english/seller/images/1/architecture-modern-residence6.jpg';?>" alt="architecture-modern-residence6" class="img-fluid d-block">
-            <div class="footer">
-                <div class="d-flex">
-                    <div class="signature horizontal_sign">
-                        <img src="https://i.ibb.co/qyHKv61/laura-chouette-nk-Wnc-W-GP8-unsplash.jpg"
-                            alt="laura-chouette-nk-Wnc-W-GP8-unsplash" class="profile_img">
-                        <div>
-                            <div class="profile_name">First Last</div>
-                            <div class="profile_title">Title</div>
-                            <a class="tel_number" href="tel:000.000.0000">O <span>000.000.0000</span></a>
-                            <a class="tel_number" href="tel:000.000.0000">M <span>000.000.0000</span></a>
-                            <a href="mailto:name@domain.com" class="contact_info">name@domain.com</a>
-                            <a href="https://domain.com" class="contact_info">domain.com</a>
-                        </div>
-                    </div>
-                    <div class="logo">
-                        <img src="<?php echo base_url().'assets/reports/english/seller/images/1/white-logo.png';?>" alt="white-logo" border="0">
-                    </div>
+        <page class="pdf1">
+            <div class="header">
+                <img src="https://i.ibb.co/6rwb4z9/light-house.png" alt="light-house" border="0">
+                <img src="https://i.ibb.co/LRwvgXQ/line.png" alt="line" border="0">
+                <div class="text">
+                    <h1>Sellers Report</h1>
+                    <p>1234 Main Street,</p>
+                    <p>City, State 00000</p>
                 </div>
             </div>
-            
-        </div>
+            <div class="body">
+                <img src="https://i.ibb.co/ZYtpFR1/architecture-modern-residence6.jpg" alt="architecture-modern-residence6" class="img-fluid d-block"><div class="footer">
+                    <div class="d-flex">
+                        <div class="signature horizontal_sign">
+                            <img src="https://i.ibb.co/qyHKv61/laura-chouette-nk-Wnc-W-GP8-unsplash.jpg"
+                                alt="laura-chouette-nk-Wnc-W-GP8-unsplash" class="profile_img">
+                            <div>
+                                <div class="profile_name">First Last</div>
+                                <div class="profile_title">Title</div>
+                                <a class="tel_number" href="tel:000.000.0000">O <span>000.000.0000</span></a>
+                                <a class="tel_number" href="tel:000.000.0000">M <span>000.000.0000</span></a>
+                                <a href="mailto:name@domain.com" class="contact_info">name@domain.com</a>
+                                <a href="https://domain.com" class="contact_info">domain.com</a>
+                            </div>
+                        </div>
+                        <div class="logo">
+                            <img src="https://i.ibb.co/T89Qq0d/white-logo.png" alt="white-logo" border="0">
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </page>
     </div>
-</div>
