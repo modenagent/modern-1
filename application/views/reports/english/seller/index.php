56495c650a40f62c5695eaf85ff91631647147d0
JerryHernandez <info@mylistingpitch.com>
2021-02-25 19:33:47 +0530

    Reverted changes related to PDF

--- application/views/reports/english/seller/index.php
+++ application/views/reports/english/seller/index.php
@@ -4,8 +4,8 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css2?family=Crimson+Text:wght@400;600;700&family=Open+Sans:wght@400;600;700&display=swap">
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/reports/english/seller/css/style.css") ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/reports/english/seller/css/bootstrap.min.css") ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/reports/english/seller/css/main.css") ?>">
 </head>
 
 <body>
@@ -60,14 +60,45 @@
 
 
 ?>
-  
-<!--  <style type="text/css" media="print">
+<style type="text/css">
+    .theme-bg
+    {
+        background-color: <?php echo $theme ?>;
+    }
+    .theme-clr
+    {
+        color: <?php echo $theme ?>;
+    }
+    .underline::after,.page9a .payment-table .left-col .progress-bar, .page9a .payment-table .right-col .progress-bar,.top-banner-left,.page10 .agreement-col .points li::before{
+        background-color: <?php echo $theme ?>;   
+    }
+    .page6 .page-content .insights .right-insight .gender-block .perc, .page6 .page6-content .insights .right-insight .gender-block .perc,.page7 .two-grid-wrapper .text-block-condensed h2 span p,.page9a .payment-table .left-col .total-amount .dolla-sign, .page9a .payment-table .right-col .total-amount .dolla-sign{
+        color: <?php echo $theme ?>;   
+    }
+    .sales-table-top.table > thead > tr > th{
+        border-color: <?php echo $theme ?>;   
+    }
+    .monthly-table{
+        border-bottom-color:<?php echo $theme ?>;   
+    }
+    .monthly-table2{
+        border-top-color:<?php echo $theme ?>;   
+    }
+    .ui-slider-horizontal .ui-slider-range{
+        background: <?php echo $theme ?> !important;
+    }
+    .ui-state-default, .ui-widget-content .ui-state-default, .ui-widget-header .ui-state-default, .ui-slider-handle .ui-state-default .ui-corner-all{
+        border: 1px solid <?php echo $theme ?> !important;
+        background: <?php echo $theme ?> !important;
+    }
+</style>   
+ <style type="text/css" media="print">
     div.container
     {
         page-break-after: always;
         page-break-inside: avoid;
     }
-</style> -->   
+</style>   
 
 <?php 
     if($partner && count($partner)>1) {
@@ -79,7 +110,7 @@
         $this->load->view('reports/english/seller/pages/1');
     }
 
-    /*$this->load->view('reports/english/seller/pages/2');    
+    $this->load->view('reports/english/seller/pages/2');    
     $this->load->view('reports/english/seller/pages/4');
     $this->load->view('reports/english/seller/pages/5');
     $this->load->view('reports/english/seller/pages/5b');
@@ -98,13 +129,13 @@
     $this->load->view('reports/english/seller/pages/6g', $customization_pages_data['17']);
     $this->load->view('reports/english/seller/pages/9d', $customization_pages_data['18']);
     $this->load->view('reports/english/seller/pages/11b', $customization_pages_data['19']);
-    $this->load->view('reports/english/seller/pages/15');*/
+    $this->load->view('reports/english/seller/pages/15');
 ?>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js" type="text/javascript"></script>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js" type="text/javascript"></script>
  
-<!-- <link rel="stylesheet" type="text/css" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css"> -->
+<link rel="stylesheet" type="text/css" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css">
 <script>
             
             function collision($div1, $div2) {
