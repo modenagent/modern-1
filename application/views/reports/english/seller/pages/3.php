dfe10e0bfe266897427d43d8d1d6a7ef2ab991ea
JerryHernandez <info@mylistingpitch.com>
2021-02-26 17:38:29 +0530

    Changed image pathe

--- application/views/reports/english/seller/pages/3.php
+++ application/views/reports/english/seller/pages/3.php
@@ -1,5 +1,5 @@
 <div class="container">
     <page class="pdf3">    	
-        <img src="<?php echo base_url().'assets/reports/english/seller/images/1/maxresdefault.jpg' ?>" alt="maxresdefault" border="0">
+        <img src="<?php echo base_url().'assets/reports/english/seller/images/3/maxresdefault.jpg' ?>" alt="maxresdefault" border="0">
     </page>
 </div>
