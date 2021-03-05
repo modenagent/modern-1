cfde132ad1f026ae2b4207a3555b190101f3c1ed
JerryHernandez <info@mylistingpitch.com>
2021-03-05 17:35:43 +0530

    PDF pages added 7 to 13

--- application/views/reports/english/seller/pages/4.php
+++ application/views/reports/english/seller/pages/4.php
@@ -1,26 +1,26 @@
 <page class="pdf4">
-            <h1>Our Mission Statement</h1>
-            <div class="whitebox">
-                <h2>Home Towne Real Estate</h2>
-                <p>believes that the best interests of our
-                    clients will always come fi rst. We select
-                    the most professional and ethical people
-                    to represent us as REALTORs, and are
-                    determined to provide them with the
-                    support necessary to enrich all facets
-                    of the homeownership experience. We
-                    believe in supporting property rights and
-                    have a deep belief that HOMEOWNERSHIP
-                    MATTERS to a healthy economy and
-                    community. We believe in a strong network
-                    of offi ce locations and tools available for
-                    clients to promote and market properties,
-                    navigate and search the market, and
-                    navigate each real estate transaction with
-                    positive results. Home Towne Real Estate
-                    REALTORs live where they work and we
-                    support their eff orts to develop their
-                    business and be responsible and active community advocates</p>
-            </div>
-            <img src="https://i.ibb.co/ZVV3vkH/lighthouse.png" alt="lighthouse" class="white_house">
-        </page>
+    <h1>Our Mission Statement</h1>
+    <div class="whitebox">
+        <h2>Home Towne Real Estate</h2>
+        <p>believes that the best interests of our
+            clients will always come first. We select
+            the most professional and ethical people
+            to represent us as REALTORs, and are
+            determined to provide them with the
+            support necessary to enrich all facets
+            of the homeownership experience. We
+            believe in supporting property rights and
+            have a deep belief that HOMEOWNERSHIP
+            MATTERS to a healthy economy and
+            community. We believe in a strong network
+            of office locations and tools available for
+            clients to promote and market properties,
+            navigate and search the market, and
+            navigate each real estate transaction with
+            positive results. Home Towne Real Estate
+            REALTORs live where they work and we
+            support their efforts to develop their
+            business and be responsible and active community advocates</p>
+    </div>
+    <img src="<?php echo base_url().'assets/reports/english/seller/images/4/lighthouse.png' ?>" alt="lighthouse" class="">
+</page>
