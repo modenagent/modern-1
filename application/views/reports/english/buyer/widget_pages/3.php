8b6f26e8f3df19f9d4145a7b4099c89ec5218c79
Binita Desai <binita.s@crestinfosystems.net>
2021-03-22 12:24:38 +0530

    Buyyer reports update

--- application/views/reports/english/buyer/widget_pages/3.php
+++ application/views/reports/english/buyer/widget_pages/3.php
@@ -1,12 +1,12 @@
  <page class="buyerpdf3">
     <div class="d-flex">    
         <div class="col-50">
-            <img src="<?php echo base_url().'assets/reports/english/seller/images/3/maxresdefault.jpg'; ?>" alt="maxresdefault" class="img1">
+            <img src="<?php echo base_url().'assets/reports/english/buyer/images/3/img20.png'; ?>" alt="maxresdefault" class="img1">
             <img src="<?php echo base_url().'assets/reports/english/buyer/images/3/img5.png'; ?>" alt="img5" class="img3 w100 img-fluid">
         </div>
         <div class="col-50">
             <img src="<?php echo base_url().'assets/reports/english/buyer/images/3/img6.png'; ?>" alt="img6" class="img2 w100 img-fluid">
-            <img src="<?php echo base_url().'assets/reports/english/seller/images/3/maxresdefault.jpg'; ?>" alt="maxresdefault" class="img1 last_img">
+            <img src="<?php echo base_url().'assets/reports/english/buyer/images/3/img21.png'; ?>" alt="maxresdefault" class="img1">
         </div>
     </div>
 </page>
