e6c330a7021fa999f4139ed89e0f2e895e082ac1
Jerry <info@clixmedia.io>
2021-04-01 09:52:59 -0700

    Styling.

--- widget/cma.js
+++ widget/cma.js
@@ -6,7 +6,7 @@ location.href = app_main_url;
 // function loadWidget()
 // {
 //     // var custom_css = "<style>#cma-widget-container {background: url("+base_url+"/../assets/images-2/home/->ReplaceImage<-) no-repeat 0 0;background-attachment: scroll; background-color:black; background-size: auto auto;background-size: cover;background-attachment: fixed;}</style>";
-//     var custom_css = "<style>#cma-widget-container {background-attachment: scroll; background-color:black; background-size: auto auto;background-size: cover;background-attachment: fixed;}</style>";
+//     var custom_css = "<style>#cma-widget-container {background-attachment: scroll; background-color:black; background-size: auto auto;background-size: cover;background-attachment: fixed; background-image: url('https://mcusercontent.com/b10d88eb10799345e0303a43d/images/7218d6f3-e7b7-4051-a604-9f43ceaaf4fc.jpg'); height:820px;}</style>";
 
 //     $.ajax({
 //         url: base_url+'/../getWidgetData',
