c9a19241ddb9674ff417e55e21b65e3464e5fbd2
Binita Desai <binita.s@crestinfosystems.net>
2021-04-27 12:02:41 +0530

    SC buyer reports

---
+++ application/views/reports/widget/maxa_dawn_cordiner/buyer/pages/13.php
@@ -0,0 +1,49 @@
+<page class="pdf13">
+    <h1 class="small_big"><span>Who Pays</span> What</h1>
+    <p class="mt-30">
+        The costs and charges of a Southern California real estate transaction are fully negotiable between the
+        Buyer and Seller through their respective agents. The negotiated terms will be set forth accordingly in the
+        Purchase Agreement.
+    </p>
+    <div class="d-flex mt-50">
+        <div class="col-50">
+            <h4 class="table_title no_line m-0">The Seller</h4>
+            <h4 class="sub_title mt-0">Can Generally Expect To Pay</h4>
+            <ul class="f14">
+                <li>
+                    Real Estate Commission Property
+                    Taxes
+                </li>
+                <li>
+                    Transfer Tax
+                </li>
+                <li>
+                    Payoff of all loans & liens Home
+                    warranty plan Escrow fee (Seller’s
+                    portion)
+                </li>
+            </ul>
+            <h4 class="table_title no_line mb-0 mt-40">The Buyer</h4>
+            <h4 class="sub_title mt-0">Can Generally Expect To Pay</h4>
+            <ul class="f14">
+                <li>
+                    Loan Origination Fee Points (If
+                    Applicable) Application Fee
+                </li>
+                <li>
+                    Escrow Fee (Buyers Portion)
+                    Appraisal Fee
+                </li>
+                <li>
+                    Home Inspection
+                </li>
+                <li>Title Insurance</li>
+                <li>Pro-Rate Taxes Homeowners Insurance</li>
+            </ul>
+            <img src="<?php echo base_url("assets/reports/widget/$report_dir_name/$presentation_type/images/img14.png"); ?>" alt="img14" class="img-fluid mt-30">
+        </div>
+        <div class="col-50">
+            <img src="<?php echo base_url("assets/reports/widget/$report_dir_name/$presentation_type/images/img15.png"); ?>" alt="img15" class="img-fluid img_700 long_img">
+        </div>
+    </div>
+</page>
---
+++ application/views/reports/widget/maxa_dawn_cordiner/buyer/pages/19.php
@@ -0,0 +1,146 @@
+<page class="pdf18">
+    <h1 class="main_title top_title">COMMON TERMS</h1>
+    <div class="d-flex mt-30">
+        <div class="col-30">
+            <h4 class="table_title no_line">CONTINGENCIES</h4>
+            <div class="grey_line"></div>
+            <p class="small_text">
+                A contingency is a condition that
+                must be met before the contract
+                between the buyer and the seller
+                becomes legally binding.
+            </p>
+            <h4 class="table_title no_line">CLOSING DISCLOSURE</h4>
+            <div class="grey_line"></div>
+            <p class="small_text">
+                A Closing Disclosure is a fivepage form that provides final
+                details about the mortgage loan
+                you have selected. It includes
+                the loan terms, your projected
+                monthly payments, and how
+                much you will pay in fees and
+                other costs to get your mortgage
+                (closing costs)
+            </p>
+            <h4 class="table_title no_line">DISCLOSURES</h4>
+            <div class="grey_line"></div>
+            <p class="small_text">
+                The making known of a fact that
+                had previously been hidden. For
+                example, a home seller must
+                disclose major physical defects
+                in a house within his or her
+                knowledge.
+            </p>
+            <h4 class="table_title no_line">EARNEST MONEY
+            DEPOSIT</h4>
+            <div class="grey_line"></div>
+            <p class="small_text">
+                The earnest money deposit is the
+                money you provide along with
+                your offer on a house as a show
+                of good faith.
+            </p>
+            <h4 class="table_title no_line">HAZARD INSURANCE</h4>
+            <div class="grey_line"></div>
+            <p class="small_text">
+                A form of insurance in which the
+                insurance company protects the
+                insured from specified losses,
+                such as fire, windstorm and the
+                like.
+            </p>
+        </div>
+        <div class="col-30">
+            <h4 class="table_title no_line">OFFERS & CONTRACTS</h4>
+            <div class="grey_line"></div>
+            <p class="small_text">
+                Once you find the right home,
+                you’ll make an offer on the
+                property with the help of an
+                agent or attorney. If the seller
+                counters your original offer, it’s
+                usually because they want more
+                money or a faster timeline for
+                closing the deal, at which point
+                you’ll have to negotiate. When
+                submitting an offer, it’s a good
+                idea to add a personal touch
+                by including a cover letter that
+                explains why you want to buy
+                the home.
+            </p>
+            <h4 class="table_title no_line">CLOSING COSTS</h4>
+            <div class="grey_line"></div>
+            <p class="small_text">
+                Closing costs are the various
+                fees, charges and taxes needed
+                to (A) originate a mortgage loan
+                and (B) transfer a property from
+                seller to buyer.
+            </p>
+            <h4 class="table_title no_line">FIXED RATES
+            & ADJUSTABLE
+            MORTGAGES</h4>
+            <div class="grey_line"></div>
+            <p class="small_text">
+                A fixed rate mortgage has the
+                same interest rate throughout
+                the life of the loan. An adjustable
+                rate mortgage has a variable
+                interest rate that changes after a
+                predetermined amount of years.
+            </p>
+            <h4 class="table_title no_line">DAYS ON MARKET</h4>
+            <div class="grey_line"></div>
+            <p class="small_text">
+                (DOM) stands for Days on market
+                and it tells you how long a home
+                has been for sale.
+            </p>
+        </div>
+        <div class="col-30">
+            <h4 class="table_title no_line">PRE-APPROVAL LETTER</h4>
+            <div class="grey_line"></div>
+            <p class="small_text">
+                Used by a lender to give you an
+                idea of the mortgage amount for
+                which you qualify for.
+            </p>
+            <h4 class="table_title no_line">LOAN ESTIMATE</h4>
+            <div class="grey_line"></div>
+            <p class="small_text">
+                The form provides you with
+                important information,
+                including the estimated interest
+                rate,monthly payment, and total
+                closing costs for the loan.
+            </p>
+            <h4 class="table_title no_line">UNDERWRITING</h4>
+            <div class="grey_line"></div>
+            <p class="small_text">
+                The process of evaluating a loan
+                application to determine if it
+                meets the lender’s standards.
+            </p>
+            <h4 class="table_title no_line">THE INSURANCE</h4>
+            <div class="grey_line"></div>
+            <p class="small_text">
+                Helps protect the homeowner
+                and buyer from fraud or forging
+                of documents that directly
+                affect ownership interest of the
+                property.
+            </p>
+            <h4 class="table_title no_line">EARNEST MONEY
+            DEPOSIT</h4>
+            <div class="grey_line"></div>
+            <p class="small_text">
+                The earnest money deposit is the
+                money you provide along with
+                your offer on a house as a show
+                of good faith.
+            </p>
+        </div>
+    </div>
+</page>
