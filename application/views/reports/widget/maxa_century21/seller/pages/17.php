9eac527d6a8242682d2fd301db88c1e785210049
Binita Desai <binita.s@crestinfosystems.net>
2021-05-04 12:44:13 +0530

    Seller remaining reports

---
+++ application/views/reports/widget/maxa_century21/seller/pages/17.php
@@ -0,0 +1,5 @@
+    <page class="pdf16">
+        <div class="big_title">RY: 1971 - 2021</div>
+        <img src='<?php echo base_url("assets/reports/widget/$report_dir_name/$presentation_type/images/pdf17.png");?>' alt="pdf17" class="img-fluid"> 
+        <img src='<?php echo base_url("assets/reports/widget/$report_dir_name/$presentation_type/images/c21_gray_watermark.png");?>' alt="c21_gray_watermark" class="nmx-50">
+    </page>
