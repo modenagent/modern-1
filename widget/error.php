3f6f54d4d69635367f0b52d73ff4e9e1b79fe026
Binita Desai <binita.s@crestinfosystems.net>
2021-05-13 15:26:36 +0530

    Added message if 3rd party cookies is block

---
+++ widget/error.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Error Page</title>
+</head>
+<body>
+<?php
+if(isset($_GET['error_no'])) {
+	$error_no = $_GET['error_no'];
+	switch ($error_no) {
+		case '1':
+			echo 'Block third-party cookies is enable on your browser. Please disable it and relaod this page.';
+			break;
+		case '2':
+			if(!empty($_GET['url'])) {
+
+				$url = $_GET['url'];
+				echo 'Your browser not support iframe. Please click here '.'<a target="_blank" href="'.$url.'">'.$url.'</a>'.' to open link in new window';
+			}
+			else {
+				echo 'Your browser not support iframe.';
+			}
+			break;
+		
+		default:
+			echo 'Invalid request';
+			break;
+	}
+}
+else {
+	echo 'Invalid request';
+}
+?>
+
+</body>
+</html>
