099c57d56b52ae2e1352a2aeef2217a2d15d37f4
Binita Desai <binita.s@crestinfosystems.net>
2021-07-26 18:30:00 +0530

    Coupon changes

--- application/controllers/coupon.php
+++ application/controllers/coupon.php
@@ -33,18 +33,18 @@ class Coupon extends CI_Controller {
                             }
                         }
                     }
-                    $sql = "SELECT c.*,log.redeem_count,count(c.coupon_id_pk) from lp_coupon_mst as c 
+                    $sql = "SELECT c.coupon_id_pk,c.coupon_amt,c.end_date,c.limit_user,count(log.redeem_count) AS redeem_cnt from lp_coupon_mst as c 
                             LEFT JOIN lp_coupon_redeem_log as log ON log.coupon_id_fk = c.coupon_id_pk AND log.user_id={$userId} AND year(log.created_at) = year(curdate()) AND month(log.created_at) = month(curdate())
                             WHERE c.coupon_code = '{$coupon_code}' 
-                            GROUP BY c.coupon_code
+                            GROUP BY c.coupon_id_pk
                             HAVING (c.end_date >= '".date("Y-m-d")."' OR c.end_date IS NULL)
-                            AND (c.limit_user = 0 OR log.redeem_count IS NULL OR c.limit_user > count(log.redeem_count)) ";
-                    $sql1 = "SELECT c.*,log.redeem_count,count(c.coupon_id_pk) from lp_coupon_mst as c 
+                            AND (c.limit_user = 0 OR c.limit_user > redeem_cnt) ";
+                    $sql1 = "SELECT c.coupon_id_pk,c.coupon_amt,c.end_date,c.limit_all,count(log.redeem_count) AS redeem_cnt from lp_coupon_mst as c 
                             LEFT JOIN lp_coupon_redeem_log as log ON log.coupon_id_fk = c.coupon_id_pk AND year(log.created_at) = year(curdate()) AND month(log.created_at) = month(curdate())
                             WHERE c.coupon_code = '{$coupon_code}' 
-                            GROUP BY c.coupon_code
+                            GROUP BY c.coupon_id_pk
                             HAVING (c.end_date >= '".date("Y-m-d")."' OR c.end_date IS NULL)
-                            AND (c.limit_all = 0 OR log.redeem_count IS NULL OR c.limit_all > count(log.redeem_count)) ";
+                            AND (c.limit_all = 0 OR c.limit_all > redeem_cnt) ";
                     $query = $this->db->query($sql);
                     $result = $query->row();
                     $query1 = $this->db->query($sql1);
