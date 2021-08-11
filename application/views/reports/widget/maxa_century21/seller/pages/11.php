8f95d25579fe218c1db6f9e0e97143abc2d336bc
Binita Desai <binita.s@crestinfosystems.net>
2021-08-11 18:54:28 +0530

    Maxa C21 seller reports update

--- application/views/reports/widget/maxa_century21/seller/pages/11.php
+++ application/views/reports/widget/maxa_century21/seller/pages/11.php
@@ -1,48 +1,32 @@
-    <page class="pdf11">
-        <div class="image_hero">
-            <h2>HOW I PROPOSE <br>TO WORK WITH YOU</h2>
-            <img src='<?php echo base_url("assets/reports/widget/$report_dir_name/$presentation_type/images/pg10.jpg");?>' alt="..." class="img480">
+<page class="pdf8">
+        <div class="resume_title">MY RESUME</div>
+        <div class="resume_card">
+            <div>
+                <div class="achivement_title">
+                    <span>5</span> AWARDS
+                </div>
+                <ul class="award_list">
+                    <li>Award 1 2018</li>
+                    <li>Award 2 2019</li>
+                    <li>Award 3 2020</li>
+                    <li>Award 4 2021</li>
+                    <li>Award 5 2021</li>
+                </ul>
+                <div class="achivement_title">
+                    <span>7</span> YEARS
+                </div>  
+                <div class="achivement_title">
+                    <b>$100,000,000+</b> volume
+                </div>
+            </div>
+            <a href="#"><img src="<?php echo base_url('assets/reports/widget/'.$report_dir_name.'/'.$presentation_type.'/images');?>/century21.png" alt="century21" class="century21"></a>
+            <?php
+            if(!empty($user['profile_image']) && $user['profile_image'] != 'no') {?>
+
+            <img src="<?php echo base_url().$user['profile_image']; ?>" alt="myprofile" class="myprofile_img">
+            <?php }
+            ?>
+            <!-- <img src="<?php echo base_url('assets/reports/widget/'.$report_dir_name.'/'.$presentation_type.'/images');?>/myprofile.png" alt="myprofile" class="myprofile_img"> -->
         </div>
-        <ul>
-            <li data-count="1.">
-                Listen to your goals and
-                expectations
-            </li>
-            <li data-count="2.">
-                Understand what makes your
-                home valuable
-            </li>
-            <li data-count="3.">
-                Share why you should list your
-                home with a CENTURY 21®
-                professional
-            </li>
-            <li data-count="4.">
-                Highlight how our presence and
-                services can help you
-            </li>
-            <li data-count="5.">
-                Show a 21-point marketing plan
-                to sell your home
-            </li>
-            <li data-count="6.">
-                Determine the market value of
-                your property
-            </li>
-            <li data-count="7.">
-                Discuss optimal pricing strategies
-            </li>
-            <li data-count="8.">
-                Select the listing price for your
-                home
-            </li>
-            <li data-count="9.">
-                Share resources and tips to help
-                you get started
-            </li>
-            <li data-count="10.">
-                Answer any questions you
-                may have
-            </li>
-        </ul>
+        <img src="<?php echo base_url('assets/reports/widget/'.$report_dir_name.'/'.$presentation_type.'/images');?>/c21_watermark_white.png" alt="c21-watermark" class="img-fluid">
     </page>
---
+++ application/views/reports/widget/maxa_century21/seller/pages/13.php
@@ -0,0 +1,48 @@
+    <page class="pdf11">
+        <div class="image_hero">
+            <h2>HOW I PROPOSE <br>TO WORK WITH YOU</h2>
+            <img src='<?php echo base_url("assets/reports/widget/$report_dir_name/$presentation_type/images/pg10.jpg");?>' alt="..." class="img480">
+        </div>
+        <ul>
+            <li data-count="1.">
+                Listen to your goals and
+                expectations
+            </li>
+            <li data-count="2.">
+                Understand what makes your
+                home valuable
+            </li>
+            <li data-count="3.">
+                Share why you should list your
+                home with a CENTURY 21®
+                professional
+            </li>
+            <li data-count="4.">
+                Highlight how our presence and
+                services can help you
+            </li>
+            <li data-count="5.">
+                Show a 21-point marketing plan
+                to sell your home
+            </li>
+            <li data-count="6.">
+                Determine the market value of
+                your property
+            </li>
+            <li data-count="7.">
+                Discuss optimal pricing strategies
+            </li>
+            <li data-count="8.">
+                Select the listing price for your
+                home
+            </li>
+            <li data-count="9.">
+                Share resources and tips to help
+                you get started
+            </li>
+            <li data-count="10.">
+                Answer any questions you
+                may have
+            </li>
+        </ul>
+    </page>
