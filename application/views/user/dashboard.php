06232ae1cc5a15ab961242dc7e95b2022a64b9ae
PacificCoastTitle <ghernandez@pct.com>
2024-01-25 16:30:04 -0800

    Wizard CSS

--- application/views/user/dashboard.php
+++ application/views/user/dashboard.php
@@ -581,7 +581,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <hr>
+                        
                             <div class="clearfix" >
                             </div>
                         </article>
