3a6f69f3e9db0aeadf2b5f2d8e89542725a21d72
crestpiyush <piyush.j@crestinfosystems.net>
2024-02-20 17:37:01 +0530

    Theme 7 multiple page pdf design changes

--- application/views/reports/english/marketUpdate/7/pages/2.php
+++ application/views/reports/english/marketUpdate/7/pages/2.php
@@ -2,14 +2,13 @@
     <div class="page_container">
         <div class="pdf_page size_letter">
             <div class="pdf_header">
-                <div style="width: 100%;">
-                    <h1>HOMES FOR SALE</h1>
-                    <div class="header-subtext">SURROUNDING: 1358 5TH ST,LA VERNE,CA</div>
-                </div>
-                <!-- <div class="header-subtext">SURROUNDING: <?php echo $propertyAddress . ',' . $propertyCity . ',' . $propertyState; ?></div> -->
-                <img src="<?php echo base_url() . $user['company_logo']; ?>" alt="companyname" border="0" class="pacific_logo" alt="sign-white">
+                <h1>HOMES FOR SALE</h1>
+                <!-- <div class="header-subtext">SURROUNDING: 1358 5TH ST,LA VERNE,CA</div> -->
+
+                <div class="header-subtext">SURROUNDING: <?php echo $propertyAddress . ',' . $propertyCity . ',' . $propertyState; ?></div>
+                <!-- <img src="<?php echo base_url('assets/reports/english/marketUpdate/assets/images/sign-white.png'); ?>" class="pacific_logo" alt="sign-white"> -->
                 <?php if (!empty($user['company_logo']) && is_file(FCPATH . '/' . $user['company_logo'])): ?>
-                    <!-- <img src="<?php echo base_url('assets/reports/english/marketUpdate/assets/images/sign-white.png'); ?>" class="pacific_logo" alt="sign-white"> -->
+                    <img src="<?php echo base_url() . $user['company_logo']; ?>" alt="companyname" border="0" class="pacific_logo" alt="sign-white">
                 <?php endif;?>
             </div>
             <div class="pdf_body">
