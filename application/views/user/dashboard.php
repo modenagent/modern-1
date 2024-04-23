1d4e49428d1b68ee7386a6e2f25be55ba3271f05
PacificCoastTitle <ghernandez@pct.com>
2024-04-23 16:07:05 -0700

    Text Edit.

--- application/views/user/dashboard.php
+++ application/views/user/dashboard.php
@@ -15,7 +15,7 @@
                             </button>  -->
                             <button class="impression_block" type="button" onclick="choose_presentation('seller');">
                               <img src="<?php echo base_url(); ?>assets/new_site/img/Sample.png" alt="..." class="img-fluid d-block mx-auto">
-                              Market Analysis <span class="subtitle">Report</span>
+                              Property CMA <span class="subtitle">Report</span>
                             </button>
                             <!-- <button class="impression_block" type="button" onclick="choose_presentation('sellerCma');">
                               <img src="<?php echo base_url(); ?>assets/new_site/img/seller_presentation.png" alt="..." class="img-fluid d-block mx-auto">
@@ -27,7 +27,7 @@
                             </button>
                             <button class="impression_block" type="button" onclick="choose_presentation('registry');">
                               <img src="<?php echo base_url(); ?>assets/new_site/img/Sample3.png" alt="..." class="img-fluid d-block mx-auto">
-                              Smart Registry<span class="subtitle">Report<span class="beta">(Beta)</span></span>
+                              Smart Registry<span class="subtitle">Report<span class="beta"> (Beta)</span></span>
                             </button>
                         </div>
                     </div>
