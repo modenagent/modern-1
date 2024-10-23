ae04d19596e748f9dcba8f59a7ec91e50a71a491
crestpiyush <piyush.j@crestinfosystems.net>
2024-10-23 17:34:09 +0530

    Theme 4 issues for design in pdf

--- application/views/reports/english/seller/4/index.php
+++ application/views/reports/english/seller/4/index.php
@@ -3,6 +3,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/reports/english/seller/4/css/style.css") ?>">
 
     <style type="text/css">
